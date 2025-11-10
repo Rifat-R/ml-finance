63914f38838cbc6d83d7518b16e0c5a75475c32a
Rifat Rahman <rifatrahman2003@gmail.com>
2025-11-10 13:45:43 +0000

    Got tailwindcss working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './Output.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>My ML in Financial Mathematics Dashboard</h1>
+        <h1 className="text-red-500">My ML in Financial Mathematics Dashboard</h1>
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
