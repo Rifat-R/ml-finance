b91944a8fe64495d041519cac158a11f14cf9557
Rifat Rahman <rifatrahman2003@gmail.com>
2026-04-11 16:01:15 +0100

    feat(news) add NewsDict and fetch_news_data function

--- backend/data/fetch_data.py
+++ backend/data/fetch_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from backend.data import tiingo_client
+from typing import TypedDict
 
 
 def fetch_stock_data(
@@ -15,4 +16,14 @@ def fetch_stock_data(
     if not isinstance(data, pd.DataFrame):
         raise RuntimeError(f"Data downloaded for {ticker} is not a DataFrame")
 
+    return data
+
+
+class NewsDict(TypedDict):
+    news: str
+    date: str
+
+
+def fetch_news_data(ticker: str) -> NewsDict:
+    data: NewsDict = {"news": "example news headline", "date": "2024-01-01"}
     return data
